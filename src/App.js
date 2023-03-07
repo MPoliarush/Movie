e502967172abb01db44e64620b71e6bba9f27b78
github-marynapoliarush <merienf8118@gmail.com>
2023-03-07 12:00:22 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import img from './images/first.png'
-import icon from './images/Icon.png'
+
+import Card from './components/Card';
+
+
 
 function App() {
+
+  const listData = [
+    {
+      img:'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      name:'Rick Sanchez',
+      type:'Human'
+    },
+    {
+      img:'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      name:'Rick Sanchez',
+      type:'Human'
+    },
+    {
+      img:'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      name:'Rick Sanchez',
+      type:'Human'
+    },
+    {
+      img:'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      name:'Rick Sanchez',
+      type:'Human'
+    },
+  ]
+
   return (
     <div className="App">
       <main>
         <img className='mainIMG' src={img}/>
         <input placeholder='Filter by name...'/>
+        
+        <Card list={listData}/>
+        
       </main>
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import img from '../images/im.jpeg'
+
+const Card = (props)=>{
+    return (
+        <div className='cardsWrapper'>
+            {props.list.map(item=>{
+                return (
+                <div className="card_container">
+                    <img src={item.img}/>
+                    <p>{item.name}</p>
+                    <p>{item.type}</p>
+                </div>)
+            })}
+        </div>
+    )
+}
+export default Card
