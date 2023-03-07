cdc1ab44ba70f1277203b16cc741ca5acf165491
github-marynapoliarush <merienf8118@gmail.com>
2023-03-07 13:01:32 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import img from './images/first.png'
 
 import Card from './components/Card';
@@ -28,7 +28,21 @@ function App() {
       name:'Rick Sanchez',
       type:'Human'
     },
+    {
+      img:'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      name:'Rick Sanchez',
+      type:'Human'
+    },
   ]
+
+
+  useEffect(()=>{
+    async function getItems(){
+      const response = await fetch('https://rickandmortyapi.com/api/character')
+      const items = await response.json()
+      console.log(items)
+    }
+  },[])
 
   return (
     <div className="App">
